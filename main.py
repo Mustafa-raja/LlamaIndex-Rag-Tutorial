36aed750feea52cb02b4086fbc9dd346eeb7175f
Mustafa <mustafa.raja.here@gmail.com>
2024-08-07 20:38:34 +0500

    romeo and juliet

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from llama_index.llms.ollama import Ollama
+# from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.core import Settings
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+
+
+# os.environ["GROQ_API_KEY"] = "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"
+# from llama_index.llms.groq import Groq
+print("loading llm")
+llm = Ollama(model="llama3.1:8b", request_timeout=10000.0)
+print("loading embedder")
+embed_model = OllamaEmbedding(
+    model_name="mxbai-embed-large",
+    base_url="http://localhost:11434",
+    ollama_additional_kwargs={"mirostat": 0},
+)
+print("setting llm and embedder")
+Settings.llm = llm
+Settings.embed_model = embed_model
+print("loading data")
+documents = SimpleDirectoryReader("data").load_data()
+print("indexing data")
+index = VectorStoreIndex.from_documents(documents, show_progress=True)
+print("generating query engine")
+query_engine = index.as_query_engine(similarity_top_k=3)
+print("generating response")
+response = query_engine.query("explain the scene where juliet dies ?")
+print(response)
