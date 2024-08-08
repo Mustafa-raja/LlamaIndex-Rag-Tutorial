c2b697a99f194b0ca4408cc355870c14076b8b2d
Mustafa <mustafa.raja.here@gmail.com>
2024-08-08 16:15:21 +0500

    local persistence

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import os
+import os.path
 from llama_index.llms.ollama import Ollama
-# from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.core import Settings
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
 
-
-# from llama_index.llms.groq import Groq
 print("loading llm")
 llm = Ollama(model="llama3.1:8b", request_timeout=10000.0)
 print("loading embedder")
@@ -18,12 +20,20 @@ embed_model = OllamaEmbedding(
 print("setting llm and embedder")
 Settings.llm = llm
 Settings.embed_model = embed_model
-print("loading data")
-documents = SimpleDirectoryReader("data").load_data()
-print("indexing data")
-index = VectorStoreIndex.from_documents(documents, show_progress=True)
-print("generating query engine")
-query_engine = index.as_query_engine(similarity_top_k=3)
+PERSIST_DIR = "./storage"
+
+if not os.path.exists(PERSIST_DIR):
+    print("loading data")
+    documents = SimpleDirectoryReader("data").load_data()
+    print("indexing data")
+    index = VectorStoreIndex.from_documents(documents, show_progress=True)
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    print("Already indexed data, pulling vectors from storage")
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+query_engine = index.as_query_engine()
 print("generating response")
-response = query_engine.query("explain the scene where juliet dies ?")
+response = query_engine.query("how did romeo die, explain the scene ?")
 print(response)
